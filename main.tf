1f4587a5215aafcd8826bba0f6c6560a4868ed42
Vadym Kotliar <vadym_kotliar@fanniemae.com>
2021-03-05 21:36:22 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -193,3 +193,7 @@ resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
 }
+
+resource "aws_instance" {
+  Department = "devops"
+}
